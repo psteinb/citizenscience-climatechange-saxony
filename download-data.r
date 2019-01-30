b0ebd337e7ac3b30f15cd1fdcf2feb0c87587ffa
Peter Steinbach <steinbach@scionics.de>
2019-01-30 23:15:27 +0100

    downloading works, need to stitch that together into one large data frame for plotting

--- download-data.r
+++ download-data.r
@@ -3,8 +3,27 @@
 library(rdwd)
 
 library(readr)
+library(ggplot2)
+library(dplyr)
 
 stations = read_csv("station_ids", col_names=FALSE)
 
-#TODO: loop through all stations and run selectDWD for each station
-selected = selectDWD(id=stations)
+
+
+download_station = function(sid){
+
+		 cat("downloading",sid,"/",nrow(stations),"\n")
+		 station_link = selectDWD(id=sid, res='monthly', var='kl', per='historical')
+		 station_data = tryCatch({dataDWD(station_link)},
+					 warning = function(w){ print(paste("warning emitted on station ",sid,w)) },
+					 error = function(e){ print(paste("failed to download station ",sid,e)); return(NA) },
+					 finally = { print(paste("download station ",sid,'successfull'))  })
+		 return(station_data)
+}
+
+
+data = sapply(stations$X1,download_station)
+
+
+
+head(df)
