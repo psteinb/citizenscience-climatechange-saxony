f8b1348e71691e536e22367a510c5d59c7603a01
Peter Steinbach <steinbach@scionics.de>
2019-02-06 23:39:11 +0100

    aspect ratio adjusted

--- animate-monthly.r
+++ animate-monthly.r
@@ -9,12 +9,19 @@ library(gganimate)
 temps = read_csv("saxony-monthly-temperature.csv")
 
 
-temp_median_year = temps %>% mutate(yr= year(ymd(MESS_DATUM_BEGINN)), mo= month(ymd(MESS_DATUM_BEGINN))) %>% filter(mo == 2)%>% group_by(yr,STATIONS_ID)
+temp_median_year = temps %>% mutate(yr= as.integer(year(ymd(MESS_DATUM_BEGINN))), mo= month(ymd(MESS_DATUM_BEGINN))) %>% filter(mo == 2)%>% group_by(yr,STATIONS_ID)
 
 # https://github.com/thomasp85/gganimate
-anplot = ggplot(temp_median_year,aes(x=geo_lon,y=geo_lat,color=MO_TT)) + geom_point(size=4) + xlab("longitude") + ylab("latitude") + labs(title = 'Year: {frame_time}', x = 'longitude', y = 'latitude') +
-  transition_time(yr) +
-  ease_aes('linear')
+anplot = ggplot(temp_median_year,aes(x=geo_lon,y=geo_lat,color=MO_TT)) +
+    geom_point(size=4) +
+    theme_minimal() +
+    xlab("longitude") +
+    ylab("latitude") +
+    labs(title = 'Year: {frame_time}', x = 'longitude', y = 'latitude') +
+    transition_time(yr) +
+    ease_aes('linear') 
 
-anim = animate(anplot, renderer = magick_renderer())
-magick::image_write(anim, path="monthly-temperatures.gif")
+anim = animate(anplot, nframes=15,
+               renderer = gifski_renderer(), width=500,height=350)
+#magick::image_write(anim, path="monthly-temperatures.gif")
+anim_save("monthly-temperatures.gif",anim)
