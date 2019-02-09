6a8a74f2edb3070d0a6fe01dd86a639dc39a882c
Peter Steinbach <steinbach@scionics.de>
2019-02-09 22:55:27 +0100

    splitted map viz from plotting

---
+++ animate-monthly-temperatures.r
@@ -0,0 +1,34 @@
+# script can be executed with
+
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(gganimate)
+
+temps = read_csv("saxony-monthly-temperature.csv")
+
+
+temp_median_year = temps %>%
+    mutate(yr= as.integer(year(ymd(MESS_DATUM_BEGINN))), mo= month(ymd(MESS_DATUM_BEGINN))) %>%
+    #filter(mo == 2)%>%
+    group_by(yr,STATIONS_ID)
+
+# https://github.com/thomasp85/gganimate
+anplot = ggplot(temp_median_year,aes(x=mo,y=MO_TT)) +
+    geom_point() +
+    geom_jitter() +
+    theme_minimal() +
+    xlab("Monat") +
+    ylab("Durchschnitstemperatur") +
+    labs(title = 'Jahr: {frame_time}', x = 'longitude', y = 'latitude') +
+    transition_time(yr) +
+    ease_aes('linear') 
+
+anim = animate(anplot,
+                                        #nframes=15,
+               #nps=15,
+               renderer = gifski_renderer(),
+               width=700,height=500)
+#magick::image_write(anim, path="monthly-temperatures.gif")
+anim_save("monthly-temperatures.gif",anim)
